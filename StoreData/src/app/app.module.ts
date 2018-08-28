597d705aa6e1f0ac0315d1028c3467d75ba95ebc
Sujay Poojari <sujaypoojari01@gmail.com>
2018-08-28 17:24:45 +0530

    Angular Material added

---
+++ StoreData/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: [MatButtonModule]
+})
+export class AppModule {}
